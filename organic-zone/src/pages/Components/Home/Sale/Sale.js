91b6419ef666d5dbb835427e5eae316be43b94e7
Israt Munia <isratmunia000@gmail.com>
2022-05-11 19:29:20 +0600

    extra section 1

--- organic-zone/src/pages/Components/Home/Sale/Sale.js
+++ organic-zone/src/pages/Components/Home/Sale/Sale.js
@@ -4,17 +4,20 @@ import './Sale.css';
 const Sale = () => {
     return (
         <div className='summer-special'>
-            <h1>- Summer Special Fruits -</h1>
+            <h1>Summer Special Fruits!</h1>
             <div className='summer-container'>
                 <div className='summer-single-item-1'>
-                    <h1>Summer Special</h1>
+                    <h1>Taste The Summer</h1>
                 </div>
                 <div className='summer-single-item-2'>
-                    <h1>Summer Special</h1>
+                    <h1>Taste The Summer</h1>
                 </div>
                 <div className='summer-single-item-3'>
-                    <h1>Summer Special</h1>
+                    <h1>Taste The Summer</h1>
                 </div>
+            </div>
+            <div>
+                <button className='sale-btn'>Subscribe And Get Discount</button>
             </div>
         </div>
     );
