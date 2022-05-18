93073bbc7f10686d394b080ee8a68ce1631947bc
Israt Munia <isratmunia000@gmail.com>
2022-05-19 00:24:21 +0600

    modified

--- organic-zone/src/index.js
+++ organic-zone/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-
+// https://stackoverflow.com/questions/72291675/how-to-style-my-select-options-specifically-for-firefox/72291966#72291966
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
